e352428f007366d26fdde91040d7f8041c37c8a8
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-04-03 00:10:42 -0400

    Setup models for Department and Employees db

---
+++ Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace AngularApp.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+
+        public string DepartmentName { get; set; } = default!;
+    }
+}
